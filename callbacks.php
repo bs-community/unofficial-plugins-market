50101f2d5fb05c1c66b395edc185f0f450645f8e
gplane <g-plane@hotmail.com>
2017-04-12 23:16:42 +0800

    Remove old view files

--- callbacks.php
+++ callbacks.php
@@ -3,5 +3,13 @@
 return [
     App\Events\PluginWasEnabled::class => function (App\Services\PluginManager $manager) {
         $manager->uninstall('plugins-market');
+    },
+    GPlane\PluginsMarket\Events\PluginWasInstalled::class => function () {
+        if (file_exists(plugin_assets('unofficial-plugins-market', 'views/config.tpl'))) {
+            unlink(plugin_assets('unofficial-plugins-market', 'views/config.tpl'));
+        }
+        if (file_exists(plugin_assets('unofficial-plugins-market', 'views/market.tpl'))) {
+            unlink(plugin_assets('unofficial-plugins-market', 'views/market.tpl'));
+        }
     }
 ];
