a27e2a47db5a95bcf97136bb59571a93e3a605b3
gplane <g-plane@hotmail.com>
2017-04-13 08:54:50 +0800

    Detect if old plugin exists

--- callbacks.php
+++ callbacks.php
@@ -2,7 +2,9 @@
 
 return [
     App\Events\PluginWasEnabled::class => function (App\Services\PluginManager $manager) {
-        $manager->uninstall('plugins-market');
+        if ($manager->getPlugin('plugins-market')) {
+            $manager->uninstall('plugins-market');
+        }
     },
     GPlane\PluginsMarket\Events\PluginWasInstalled::class => function () {
         if (file_exists(plugin_assets('unofficial-plugins-market', 'views/config.tpl'))) {
